6daaf069bcdbf4483685c550683c487a164a40b0
thomasdussart <thomas.dussart@gmail.com>
2021-09-16 15:27:46 +0200

    ex put/patch delete

--- Exercices JS/06-Asynchrone/Exercice-2-fetch-and-Format/script.js
+++ Exercices JS/06-Asynchrone/Exercice-2-fetch-and-Format/script.js
@@ -1,9 +1,1 @@
 // `https://goweather.herokuapp.com/weather/${city}` => ${city}correspond à la ville choisie
-document.getElementById("run").addEventListener("click", () => {
-  let city = prompt("Entre une ville");
-  fetch(`https://goweather.herokuapp.com/weather/${city}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-});
