d0905aca24a822e5de1f9f197efb4c2f930019c0
thomasdussart <thomas.dussart@gmail.com>
2021-09-21 13:17:22 +0200

    add php exercice

---
+++ Exercices PHP/Exercice-form/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Sirius/PHP - Formulaires</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/styles.css" />
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Sirius Hub</h1>
+            <h2>Exercice <span id="JS">PHP</span></h2>
+        </header>
+        <div class="content">
+            <section class="explain">
+                <p>Crée un formulaire en HTML en demandant les infos suivantes :
+                <ul>
+                    <li>Nom</li>
+                    <li>Prenom</li>
+                    <li>Rue</li>
+                    <li>Ville</li>
+                    <li>Pays</li>
+                </ul>
+                Traite les données dans le fichier <em>traitement.php</em> et affiche les données sous forme de tableau.
+                </p>
+            </section>
+            <!-- INSERER ICI L'OUVERTURE DU FORM -->
+            <span class="result">Résultat</span>
+            <!-- INSERER ICI LE CONTENU DU FORMULAIRE -->
+            <!-- INSERER ICI LA FERMETURE DU FORM -->
+        </div>
+    </div>
+    <script src="./script.js"></script>
+</body>
+
+</html>
