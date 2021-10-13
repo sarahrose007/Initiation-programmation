4fff4c90afc6cf5ef8b7e4ca01e8491a7e1c5219
thomasdussart <thomas.dussart@gmail.com>
2021-10-13 13:37:34 +0200

    axejs + corrections init

---
+++ correction_JS/02-DOM/Exercice-3-Calculatrice/script.js
@@ -0,0 +1,7 @@
+document.getElementById("addition").addEventListener("click", () => {});
+
+document.getElementById("substraction").addEventListener("click", () => {});
+
+document.getElementById("division").addEventListener("click", () => {});
+
+document.getElementById("multiplication").addEventListener("click", () => {});
---
+++ correction_JS/05-Objets/Exercice-3-from-entries/script.js
@@ -0,0 +1,2 @@
+const keys = ["nom", "espece", "age", "genre", "couleur"];
+const values = ["Pixel", "chat", 3, "male", "tigré"];
